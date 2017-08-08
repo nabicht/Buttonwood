52f537d9622271bd65741b0edf06f3852ca36f99
Peter Nabicht <nabicht@gmail.com>
2017-08-08 09:46:26 -0400

    added license

--- MarketMetrics/MicroPrice.py
+++ MarketMetrics/MicroPrice.py
@@ -1,3 +1,32 @@
+"""
+This file is part of MarketPy.
+
+MarketPy is a python software package created to help quickly create, (re)build, or
+analyze markets, market structures, and market participants.
+
+MIT License
+
+Copyright (c) 2016-2017 Peter F. Nabicht
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 
 def size_weighted_midpoint(bid_price, bid_qty, ask_price, ask_qty):
     bid_plus_ask_qty = bid_qty + ask_qty
