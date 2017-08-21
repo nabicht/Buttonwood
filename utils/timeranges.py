d18fa1b329e1ae5254f6699aef86fa3690b94591
Peter Nabicht <nabicht@gmail.com>
2017-08-20 22:28:12 -0400

    made some necessary changes to timerange. Now It uses None as the end time by default. This is much much cleaner and makes using it much simpler

--- utils/timeranges.py
+++ utils/timeranges.py
@@ -31,10 +31,9 @@ from MarketPy.utils import timehelpers
 
 
 class TimeRange(object):
-    def __init__(self, start_time, end_time, payload):
-        assert end_time >= start_time, "End time (%.6f) should be greater than or equal to start time (%.6f)." % (end_time, start_time)
+    def __init__(self, start_time, payload):
         self._start_time = start_time
-        self._end_time = end_time
+        self._end_time = None
         self._payload = payload
 
     def start_time(self):
@@ -44,10 +43,18 @@ class TimeRange(object):
         return self._end_time
 
     def set_end_time(self, time):
+        if time < self._start_time:
+            raise Exception("%.6f is less than start time: %.6f" % (self._start_time, time))
         self._end_time = time
+
+    def is_closed(self):
+        return self._end_time is not None
 
     def payload(self):
         return self._payload
+
+    def __str__(self):
+        return "%.6f - %.6f : %s" % (self.start_time(), self.end_time(), self.payload())
 
 
 class TimeRangeList(object):
@@ -65,7 +72,9 @@ class TimeRangeList(object):
 
     def append(self, time_range_item):
         assert isinstance(time_range_item, TimeRange)
-        assert len(self._list) == 0 or (time_range_item.start_time() >= self._list[-1].end_time()), "New item's start time must be >= last item's end time"
+        assert len(self._list) == 0 or (not self._list[-1].is_closed()) or (time_range_item.start_time() >= self._list[-1].end_time()), "New item's start time must be >= last item's end time"
+        if len(self._list) > 0 and not self._list[-1].is_closed():
+            self.set_last_end_time(time_range_item.start_time())
         self._list.append(time_range_item)
 
     def items_in_window(self, window_start_time, window_end_time, enforce_window=True):
@@ -76,30 +85,44 @@ class TimeRangeList(object):
             # 2) end_time is between time_range_itme start and end
             # 3) the entire time_range_item falls within the window
             # if the time_range falls within the entire window, you use it and only it, whether enforcing the window or not
-            if time_range_item.start_time() >= window_start_time and time_range_item.end_time() <= window_end_time:
+            tr_start_time = time_range_item.start_time()
+            tr_end_time = time_range_item.end_time()
+            if tr_start_time >= window_start_time and time_range_item.is_closed() and tr_end_time <= window_end_time:
                 items.append(time_range_item)
             # otherwise, if there is any window overlap then you do the max and mins
-            elif (time_range_item.start_time() <= window_start_time <= time_range_item.end_time()) or (
-                    time_range_item.start_time() <= window_end_time <= time_range_item.end_time()):
+            elif time_range_item.is_closed() and \
+                    ((time_range_item.start_time() <= window_start_time <= time_range_item.end_time()) or
+                     (time_range_item.start_time() <= window_end_time <= time_range_item.end_time())):
                 if enforce_window:
-                    items.append(TimeRange(max(window_start_time, time_range_item.start_time()),
-                                           min(window_end_time, time_range_item.end_time()),
-                                           time_range_item.payload()))
+                    use_start_time = max(window_start_time, time_range_item.start_time())
+                    use_end_time = min(window_end_time, time_range_item.end_time())
+                    tr = TimeRange(use_start_time, time_range_item.payload())
+                    tr.set_end_time(use_end_time)
+                    items.append(tr)
                 else:
                     items.append(time_range_item)
+            elif not time_range_item.is_closed() and tr_start_time < window_end_time:
+                if enforce_window:
+                    use_start_time = max(window_start_time, time_range_item.start_time())
+                    use_end_time = window_end_time
+                    tr = TimeRange(use_start_time, time_range_item.payload())
+                    tr.set_end_time(use_end_time)
+                    items.append(tr)
+                else:
+                    items.append(time_range_item)
+            # TODO should make sure that anything else is ust outside of the window and closed
         return items
 
     def trim_to_start_time(self, start_time):
         trim_to = 0
-        # Because of the last time range possibly still being "open" (or added to) you can't ever delete the last thing in the list. Enforcing windows makes up for this os don't worry.
         for index, time_range_item in enumerate(self._list):
-            if time_range_item.end_time() < start_time:
+            if time_range_item.is_closed() and time_range_item.end_time() < start_time:
                 trim_to = index + 1
             else:
                 break
         if trim_to > 0:  # only bother doing it if no change
             if trim_to >= len(self._list):
-                self._list = self._list[len(self._list) - 1:]  # this way we never delete the last item
+                self._list = self._list[len(self._list) - 1:]  # this way we never delete the last item (# TODO  this might be redundant safety)
             else:
                 self._list = self._list[trim_to:]
             if len(self._list) == 0:
