f9d577966d15202c05e6019e84d6037d41d91e47
Peter Nabicht <nabicht@gmail.com>
2017-11-13 13:34:48 -0500

    cleaned up code formatting

--- tests/test_MarketObjects/test_price.py
+++ tests/test_MarketObjects/test_price.py
@@ -32,84 +32,91 @@ from MarketPy.MarketObjects.Price import Price
 from MarketPy.MarketObjects.Side import BID_SIDE
 from MarketPy.MarketObjects.Side import ASK_SIDE
 
+
 def test_setting_price():
-   p = Price("94.58793")
-   assert p.price() == Decimal("94.58793")
+    p = Price("94.58793")
+    assert p.price() == Decimal("94.58793")
+
 
 def test_better_than():
-   p = Price("91.543")
-   p_less = Price("84.456")
-   p_more = Price("92.123")
-   p_same = Price("91.543")
-   assert p_more.better_than(p, BID_SIDE) is True
-   assert p_more.better_than(p, ASK_SIDE) is False
-   assert p_less.better_than(p, BID_SIDE) is False
-   assert p_less.better_than(p, ASK_SIDE) is True
-   assert p.better_than(p_same, BID_SIDE) is False
-   assert p.better_than(p_same, ASK_SIDE) is False
+    p = Price("91.543")
+    p_less = Price("84.456")
+    p_more = Price("92.123")
+    p_same = Price("91.543")
+    assert p_more.better_than(p, BID_SIDE) is True
+    assert p_more.better_than(p, ASK_SIDE) is False
+    assert p_less.better_than(p, BID_SIDE) is False
+    assert p_less.better_than(p, ASK_SIDE) is True
+    assert p.better_than(p_same, BID_SIDE) is False
+    assert p.better_than(p_same, ASK_SIDE) is False
+
 
 def test_better_or_same_as():
-   p = Price("91.543")
-   p_less = Price("84.456")
-   p_more = Price("92.123")
-   p_same = Price("91.543")
-   assert p_more.better_than(p, BID_SIDE) is True
-   assert p_more.better_than(p, ASK_SIDE) is False
-   assert p_less.better_or_same_as(p, BID_SIDE) is False
-   assert p_less.better_or_same_as(p, ASK_SIDE) is True
-   assert p.better_or_same_as(p_same, BID_SIDE) is True
-   assert p.better_or_same_as(p_same, ASK_SIDE) is True
+    p = Price("91.543")
+    p_less = Price("84.456")
+    p_more = Price("92.123")
+    p_same = Price("91.543")
+    assert p_more.better_than(p, BID_SIDE) is True
+    assert p_more.better_than(p, ASK_SIDE) is False
+    assert p_less.better_or_same_as(p, BID_SIDE) is False
+    assert p_less.better_or_same_as(p, ASK_SIDE) is True
+    assert p.better_or_same_as(p_same, BID_SIDE) is True
+    assert p.better_or_same_as(p_same, ASK_SIDE) is True
+
 
 def test_worse_than():
-   p = Price("91.543")
-   p_less = Price("84.456")
-   p_more = Price("92.123")
-   p_same = Price("91.543")
-   assert p_less.worse_than(p, BID_SIDE) is True
-   assert p_less.worse_than(p, ASK_SIDE) is False
-   assert p_more.worse_than(p, BID_SIDE) is False
-   assert p_more.worse_than(p, ASK_SIDE) is True
-   assert p.worse_than(p_same, BID_SIDE) is False
-   assert p.worse_than(p_same, ASK_SIDE) is False
+    p = Price("91.543")
+    p_less = Price("84.456")
+    p_more = Price("92.123")
+    p_same = Price("91.543")
+    assert p_less.worse_than(p, BID_SIDE) is True
+    assert p_less.worse_than(p, ASK_SIDE) is False
+    assert p_more.worse_than(p, BID_SIDE) is False
+    assert p_more.worse_than(p, ASK_SIDE) is True
+    assert p.worse_than(p_same, BID_SIDE) is False
+    assert p.worse_than(p_same, ASK_SIDE) is False
+
 
 def test_worse_or_same_as():
-   p = Price("91.543")
-   p_less = Price("84.456")
-   p_more = Price("92.123")
-   p_same = Price("91.543")
-   assert p_less.worse_or_same_as(p, BID_SIDE) is True
-   assert p_less.worse_or_same_as(p, ASK_SIDE) is False
-   assert p_more.worse_or_same_as(p, BID_SIDE) is False
-   assert p_more.worse_or_same_as(p, ASK_SIDE) is True
-   assert p.worse_or_same_as(p_same, BID_SIDE) is True
-   assert p.worse_or_same_as(p_same, ASK_SIDE) is True
+    p = Price("91.543")
+    p_less = Price("84.456")
+    p_more = Price("92.123")
+    p_same = Price("91.543")
+    assert p_less.worse_or_same_as(p, BID_SIDE) is True
+    assert p_less.worse_or_same_as(p, ASK_SIDE) is False
+    assert p_more.worse_or_same_as(p, BID_SIDE) is False
+    assert p_more.worse_or_same_as(p, ASK_SIDE) is True
+    assert p.worse_or_same_as(p_same, BID_SIDE) is True
+    assert p.worse_or_same_as(p_same, ASK_SIDE) is True
+
 
 def test_comparison():
-   p = Price("91.543")
-   p_less = Price('84.456')
-   p_more = Price('92.123')
-   p_same = Price('91.543')
-   assert p_less < p_more
-   assert p_less <= p_more
-   assert p_less != p_more
-   assert (p_less > p_more) is False
-   assert (p_less >= p_more) is False
-   assert (p_less == p_more) is False
-   assert p >= p_same
-   assert p <= p_same
-   assert p == p_same
-   assert (p != p_same) is False
-   assert p_same >= p
-   assert p_same <= p
+    p = Price("91.543")
+    p_less = Price('84.456')
+    p_more = Price('92.123')
+    p_same = Price('91.543')
+    assert p_less < p_more
+    assert p_less <= p_more
+    assert p_less != p_more
+    assert (p_less > p_more) is False
+    assert (p_less >= p_more) is False
+    assert (p_less == p_more) is False
+    assert p >= p_same
+    assert p <= p_same
+    assert p == p_same
+    assert (p != p_same) is False
+    assert p_same >= p
+    assert p_same <= p
+
 
 def test_price_math():
-   p1 = Price('1.15')
-   p2 = Price('1.35')
-   p3 = Price('0.01')
-   assert p1 + p2 == Decimal('2.5')
-   assert p2 + p1 == Decimal('2.5')
-   assert p2 - p1 == Decimal('.2')
-   assert p1 - p2 == Decimal('-0.2')
-   assert p1 * p2 == Decimal('1.5525')
-   assert p2 * p1 == Decimal('1.5525')
-   assert p1 / p3 == Decimal('115')
+    p1 = Price('1.15')
+    p2 = Price('1.35')
+    p3 = Price('0.01')
+    assert p1 + p2 == Decimal('2.5')
+    assert p2 + p1 == Decimal('2.5')
+    assert p2 - p1 == Decimal('.2')
+    assert p1 - p2 == Decimal('-0.2')
+    assert p1 * p2 == Decimal('1.5525')
+    assert p2 * p1 == Decimal('1.5525')
+    assert p1 / p3 == Decimal('115')
