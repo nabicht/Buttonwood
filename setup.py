4235943b71dcf675619c08ab3dee31488543804d
Peter F. Nabicht <nabicht@gmail.com>
2019-07-21 18:35:39 -0500

    exclude examples from package and prepping for 1.1.1

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import find_packages
 
 setup(
   name = 'buttonwood',
-  packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-  version = '1.0.6',
+  packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests",
+                                    "*.exmaples", "*.examples.*", "examples.*", "examples"]),
+  version = '1.1.1',
   license='MIT',
   description = 'Buttonwood is a python software package created to help quickly create, (re)build, or analyze markets, market structures, and market participants.',
   author = 'Peter F. Nabicht',
   author_email = 'nabicht@gmail.com',
   url = 'https://github.com/nabicht/Buttonwood',
-  download_url = 'https://github.com/nabicht/Buttonwood/archive/v1.0.6.tar.gz',
+  download_url = 'https://github.com/nabicht/Buttonwood/archive/v1.1.1.tar.gz',
   keywords = ['markets', 'finance', 'electronic markets', 'microstructure', 'market analysis'],
   install_requires=[
           'nose',
