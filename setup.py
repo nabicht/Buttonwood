5d40ad9548993fd468f8a0ffabac9c7a81cbc275
Peter F. Nabicht <nabicht@gmail.com>
2019-09-12 14:37:32 -0500

    prepping for 1.0.7

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name = 'buttonwood',
   packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-  version = '1.0.6',
+  version = '1.0.7',
   license='MIT',
   description = 'Buttonwood is a python software package created to help quickly create, (re)build, or analyze markets, market structures, and market participants.',
   author = 'Peter F. Nabicht',
   author_email = 'nabicht@gmail.com',
   url = 'https://github.com/nabicht/Buttonwood',
-  download_url = 'https://github.com/nabicht/Buttonwood/archive/v1.0.6.tar.gz',
+  download_url = 'https://github.com/nabicht/Buttonwood/archive/v1.0.7.tar.gz',
   keywords = ['markets', 'finance', 'electronic markets', 'microstructure', 'market analysis'],
   install_requires=[
           'nose',
