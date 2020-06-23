f86098cd6d97b4767cbd3428e667b307255ba571
Ian Hutchinson <ihutchinson@agalmicventures.com>
2020-06-23 13:38:21 -0500

    Update copyrights for 2020 (automated edit)

--- buttonwood/MarketObjects/OHLCV.py
+++ buttonwood/MarketObjects/OHLCV.py
@@ -7,7 +7,7 @@ analyze markets, market structures, and market participants.
 
 MIT License
 
-Copyright (c) 2016-2019 Peter F. Nabicht
+Copyright (c) 2016-2020 Peter F. Nabicht
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
